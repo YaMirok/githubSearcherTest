34ffd7585d4f8db7a910a8a04be1e9b50a625184
Damir <sitdikovd.a@gmail.com>
2021-08-15 11:13:16 +0500

    Initial Commit

---
+++ GithubSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubSearcher
+//
+//  Created by Damir Sitdikov on 15.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
